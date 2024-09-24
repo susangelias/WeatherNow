7631253ead78a61d653d3231829bb5e4f4b62074
susangelias <susangelias@gmail.com>
2024-09-24 15:41:04 -0400

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Susan Elias on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
