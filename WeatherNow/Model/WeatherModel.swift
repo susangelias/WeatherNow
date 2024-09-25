308e54b844beff899474998e0d1fb4c856b93d56
susangelias <susangelias@gmail.com>
2024-09-25 12:05:22 -0400

    Move api key into config files

--- WeatherNow/Model/WeatherModel.swift
+++ WeatherNow/Model/WeatherModel.swift
@@ -20,46 +20,51 @@ class WeatherModel: ObservableObject {
     @Published var error: String?
 
     private var cancellables = Set<AnyCancellable>()
-    private let apiKey = "4387d64fd8cd9d7b80504b7a32bc1bdb" // Replace with your actual API key
     
     func fetchWeather(latitude: Double, longitude: Double, units: TemperatureUnit) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=\(units.rawValue)&appid=\(apiKey)"
-        guard let url = URL(string: urlString) else {
-            self.error = "Invalid URL"
-            return
+        
+        print("Bundle.main.infoDictionary \(String(describing: Bundle.main.infoDictionary))")
+        if let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String {
+            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=\(units.rawValue)&appid=\(apiKey)"
+            guard let url = URL(string: urlString) else {
+                self.error = "Invalid URL"
+                return
+            }
+            
+            print("Fetching weather from URL: \(urlString)")
+            
+            URLSession.shared.dataTaskPublisher(for: url)
+                .map(\.data)
+                .tryMap { data -> Data in
+                    // Print raw JSON data
+                    if let jsonString = String(data: data, encoding: .utf8) {
+                        print("Raw JSON respone")
+                        print(jsonString)
+                    }
+                    return data
+                }
+                .decode(type: APIResponse.self, decoder: JSONDecoder())
+                .receive(on: DispatchQueue.main)
+                .sink { completion in
+                    if case .failure(let error) = completion {
+                        print("Error: \(error)")
+                        
+                        self.error = "API Error: \(error.localizedDescription)"
+                    }
+                } receiveValue: { response in
+                    switch response {
+                    case .success(let weatherData):
+                        self.temperature = weatherData.main.temp
+                        self.description = weatherData.weather.first?.description
+                        self.error = nil
+                    case .failure(let errorResponse):
+                        self.error = "API Error: \(errorResponse.message)"
+                    }
+                }
+                .store(in: &cancellables)
+        } else {
+            print("WeatherModel missing api key")
         }
-
-        print("Fetching weather from URL: \(urlString)")
-
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map(\.data)
-            .tryMap { data -> Data in
-                // Print raw JSON data
-                if let jsonString = String(data: data, encoding: .utf8) {
-                    print("Raw JSON respone")
-                    print(jsonString)
-                }
-                return data
-            }
-            .decode(type: APIResponse.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .sink { completion in
-                if case .failure(let error) = completion {
-                    print("Error: \(error)")
-
-                    self.error = "API Error: \(error.localizedDescription)"
-                }
-            } receiveValue: { response in
-                switch response {
-                 case .success(let weatherData):
-                     self.temperature = weatherData.main.temp
-                     self.description = weatherData.weather.first?.description
-                     self.error = nil
-                 case .failure(let errorResponse):
-                     self.error = "API Error: \(errorResponse.message)"
-                 }
-            }
-            .store(in: &cancellables)
     }
 }
 
